a0c53fed139d595ea56faf4174bb0543bf44f600
samuel siahaan <samuelsiahaan476@gmail.com>
2026-09-16 16:29:19 +0700

    menambahkan index.html

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const greetingButton =
+ document.querySelector('#greeting-button');
+greetingButton.addEventListener('click', () => {
+ alert('selamat melihat wep saya');
+ console. log('main.js berhasil dijalankan');
+});
